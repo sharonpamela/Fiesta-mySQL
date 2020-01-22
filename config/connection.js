6f2f496279045c6bcad4a46daa469a42c364ae59
sharonpamela <sharon.pamela.santana@gmail.com>
2020-01-22 11:29:27 -0800

    changed db passwd

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,75 @@
 const mysql = require('mysql');
+
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'nutanix/4u',
+    password: 'db_passwd',
     database: 'inventoryManagement'
 });
 
 // const sql = require('mssql');
-// const config = {
+// // config for your database
+// var config = {
 //     user: 'administrator',
 //     password: 'nutanix/4u',
-//     server: '10.42.101.54',
+//     server: '10.42.101.54\\Fiesta-DB', 
+//     database: 'inventoryManagement' 
+// };
+
+// // connect to your database
+// sql.connect(config, function (err) {
+
+//     if (err) console.log(err);
+
+//     // create Request object
+//     var request = new sql.Request();
+       
+//     // query to the database and get the records
+//     request.query('select * from Student', function (err, recordset) {
+        
+//         if (err) console.log(err)
+
+//         // send records as a response
+//         res.send(recordset);
+        
+//     });
+// });
+
+
+
+
+
+
+// const dbConfig = {
+//     server: '10.42.101.54\\Fiesta-DB',
 //     database: 'Fiesta-DB',
+//     user: 'administrator',
+//     password: 'nutanix/4u',
 //     port: 1433
 // }
 
-// const connection = sql.connect(config);
+// function getData() {
+//     let conn = new sql.Connection(dbConfig);
+//     let req = new sql.Request(conn);
+
+//     conn.connect(function (err) {
+//         if (err) {
+//             console.log(err);
+//             return;
+//         }
+//         req.query("Select * from stores", function (err, recordset) {
+//             if (err) {
+//                 console.log(err);
+//             } else {
+//                 console.log(recordset);
+//             }
+//             conn.close();
+//         });
+//     });
+// }
+
 
 module.exports = connection;
 
+// sqlcmd -S 127.0.0.1 -U sa -P your_password -Q "SELECT @@VERSION"
--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// if(process.env.NODE_ENV === 'production') {
-//     app.use(express.static('client/build'));
-// }
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -23,5 +23,5 @@ const routes = require('./routes');
 // Prepend / to any route declared inside of routes
 app.use(routes);
 
-app.listen(PORT, () => console.log('Port started on port: ' + PORT));
 
+app.listen(PORT, () => console.log('Port started on port: ' + PORT));
