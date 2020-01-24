15ef56395c83363fc66dc3de2e94752a3c7a4f9c
sharonpamela <sharon.pamela.santana@gmail.com>
2020-01-24 00:01:45 +0000

    made it work with outside DB and cleaned up code a bit

---
+++ client/build/precache-manifest.e560244b9251d5d6c459d705347bf8d1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f89899e45716a48bf22b884a93dc2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cfe4168d716c3ea02ce",
+    "url": "/static/css/main.5de2b644.chunk.css"
+  },
+  {
+    "revision": "3da485588b8100949c2a",
+    "url": "/static/js/2.9d3a83cb.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.9d3a83cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2cfe4168d716c3ea02ce",
+    "url": "/static/js/main.c377922b.chunk.js"
+  },
+  {
+    "revision": "5274233e5f14eabd9ee9",
+    "url": "/static/js/runtime-main.38a53adb.js"
+  },
+  {
+    "revision": "9044b10c83aa77b50e6a0fd9f732061e",
+    "url": "/static/media/inventory.9044b10c.png"
+  },
+  {
+    "revision": "c4a933274b3b18a7dd8997beec61b2c6",
+    "url": "/static/media/products.c4a93327.png"
+  },
+  {
+    "revision": "2e5d8723ab8edc917ca14b075cdfa3eb",
+    "url": "/static/media/stores.2e5d8723.png"
+  }
+]);
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const path = require("path");
 const PORT = process.env.PORT|| 3001;
 
 const  cors = require("cors");
@@ -9,9 +9,20 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
+// if(process.env.NODE_ENV === 'production') {
+//    console.log("Running on production");
+//    app.use(express.static('client/build'));
+//}
+
+const connectHistoryApiFallback = require('connect-history-api-fallback');
+
+
+app.use(connectHistoryApiFallback({
+  verbose: false
+}));
+
+// static files and folders must be set after connectHistoryApiFallback
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use(cors());
 app.use(bodyParser.json());
